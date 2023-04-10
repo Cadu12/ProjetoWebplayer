311613d7b9245b1de8dfcd21d11be741d9fd5ec0
Carlos Oliveira <cadu.oliveira@yahoo.com.br>
2023-04-10 16:22:35 -0300

    Banco configurado.

--- src/main/java/br/com/projeto/webplayer/Application.java
+++ src/main/java/br/com/projeto/webplayer/Application.java
@@ -2,8 +2,14 @@ package br.com.projeto.webplayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.com.projeto.webplayer.*"})
+@EntityScan(basePackages = {"br.com.projeto.webplayer.*"})
+@EnableJpaRepositories(basePackages = {"br.com.projeto.webplayer.*"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
