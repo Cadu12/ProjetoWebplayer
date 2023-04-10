20a4aa6d1ba9d2415a5b7d74504e897a76ad84a4
Carlos Oliveira <cadu.oliveira@yahoo.com.br>
2023-04-10 00:24:37 -0300

    Initial commit

---
+++ src/main/frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["../resources/templates/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
