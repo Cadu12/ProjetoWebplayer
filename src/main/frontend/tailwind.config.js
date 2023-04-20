c78ea8150f40c07935df7af5c84136e8cfa65231
Carlos Oliveira <cadu.oliveira@yahoo.com.br>
2023-04-20 02:07:40 -0300

    Tirar nodejs

--- src/main/frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: ["../resources/templates/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
